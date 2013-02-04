ffb3777e1c7356043c890d41f460e404222fb3dd
Nicolas Leroux <nicolas.leroux@outlook.com>
2013-02-04 16:09:54 -0600

    Display now displays balls as a circle, and some balls are created on initialization

--- project2/main.cpp
+++ project2/main.cpp
@@ -8,15 +8,48 @@
 using namespace std;
 
 vector<Ball> balls;
-Ball cueBall();
+Ball cueBall;
+
+// displays a ball object on the screen
+void DisplayBall(Ball toDisplay) {
+
+  switch (toDisplay.color) {
+    case WHITE:
+      glColor3f(1.0, 1.0, 1.0);
+      break;
+    case BLACK:
+      glColor3f(0.0, 0.0, 0.0);
+      break;
+    case RED:
+      glColor3f(1.0, 0.0, 0.0);
+      break;
+    case GREEN:
+      glColor3f(0.0, 1.0, 0.0);
+    case BLUE:
+      glColor3f(0.0, 0.0, 1.0);
+      break;
+    default:
+      glColor3f(0.0, 0.0, 0.0);
+      break;
+  }
+
+  glBegin(GL_POLYGON);
+  for(double i = 0; i < 2 * PI; i += PI / 6) {
+    glVertex2f(toDisplay.x + cos(i) * BALL_RADIUS,
+      toDisplay.y + sin(i) * BALL_RADIUS);
+  }
+
+  glEnd();
+}
 
 void Display() {
   glClear(GL_COLOR_BUFFER_BIT);
 
-  glBegin(GL_POINTS);
-  glColor3f(0.0, 0.0, 0.0);
-  glVertex2f(0, 0);
-  glEnd();
+  DisplayBall(cueBall);
+
+  for (int i = 0; i < balls.size(); i += 1) {
+    DisplayBall(balls[i]);
+  }
 
   glFlush();
 }
@@ -34,7 +67,7 @@ void Init(int argc, char** argv) {
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
   glutInitWindowSize(800, 600);
   glutInitWindowPosition(0, 0);
-  // DO NOT change the window title.  It is necessary for the screen capture.
+
   glutCreateWindow("Billiards");
   glutMouseFunc(Mouse);
   glutDisplayFunc(Display);
@@ -46,11 +79,19 @@ void Init(int argc, char** argv) {
   glLoadIdentity();
   gluOrtho2D(-200.0, 200.0, -150.0, 150.0);
   glMatrixMode(GL_MODELVIEW);
-
-  glutMainLoop();
 }
 
 
 int main(int argc, char** argv) {
   Init(argc, argv);
+
+  // make a white cue  ball
+  cueBall = Ball(-50, 0, WHITE);
+
+  // make some game balls to be knocked around
+  balls.push_back(Ball(50, 25, RED));
+  balls.push_back(Ball(50, 0, BLACK));
+  balls.push_back(Ball(50, -25, BLUE));
+
+  glutMainLoop();
 }
