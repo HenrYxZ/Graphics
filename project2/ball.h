d84186ef01cee1d8fab41d721933d16f2c08c33e
Nicolas Leroux <nicolas.leroux@outlook.com>
2013-02-04 15:13:32 -0600

    meant to include in last push

--- project2/ball.h
+++ project2/ball.h
@@ -3,10 +3,29 @@
 
 #include "./geom2.h"
 
-#define BALL_WIDTH 10
+#define BALL_RADIUS 10
+
+enum Color { WHITE, BLACK, RED, GREEN, BLUE }; 
 
 // A handy Ball class
 class Ball {
+
+public:
+  // the current position of the ball
+  int x, y;
+
+  // the color of the ball as defined by the color enumeration
+  Color color;
+
+  struct velocity {
+    int x;
+    int y;
+  };
+
+  Ball();
+
+  Ball(int x, int y, Color color);
+
 };
 
 #endif
