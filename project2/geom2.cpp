9b7b4ff5ecb38acf41851c83c31ee9ab948c9d51
Cameron Hooper <cs.utexas.edu>
2013-02-07 22:23:14 -0600

    Fleshed out the Velocity class and DotProduct and AngleBetween.

--- project2/geom2.cpp
+++ project2/geom2.cpp
@@ -3,3 +3,57 @@
 int distance(int xA, int yA, int xB, int yB) {
   return sqrt(pow((xB - xA), 2) + pow((yB - yA), 2));
 }
+
+int pythagorean(int x, int y) {
+  return sqrt(pow(x, 2) + pow(y, 2));
+}
+
+float RadtoDeg(float rad) {
+  return rad * 180 / 3.14159;
+}
+
+float DegtoRad(float deg) {
+  return deg * 3.14159 / 180;
+}
+
+Velocity::Velocity() {
+  this->x = 1;
+  this->y = 1;
+  Velocity::setDirectionRad();
+  Velocity::setMagnitude();
+}
+
+Velocity::Velocity(int x, int y) {
+  this->x = x;
+  this->y = y;
+  Velocity::setDirectionRad();
+  Velocity::setMagnitude();
+}
+
+// setting new x and y values will adjust the direction
+// and magnitude accordingly
+void Velocity::setXY(int x, int y) {
+  this->x = x;
+  this->y = y;
+  Velocity::setDirectionRad();
+  Velocity::setMagnitude();
+}
+
+void Velocity::slow(int t) {
+  magnitude += ACCELERATION * t;
+  updateXY();
+}
+
+// After slowing/reducing the magnitude, updates x and y
+void Velocity::updateXY() {
+  x = cos(directionRad) * magnitude;
+  y = sin(directionRad) * magnitude;
+}
+
+int DotProduct(Velocity vA, Velocity vB) {
+  return vA.getX() + vA.getY() + vB.getX() + vB.getY(); 
+}
+
+float AngleBetween(Velocity vA, Velocity vB) {
+  return acos(DotProduct(vA, vB) / (vA.getMagnitude() * vB.getMagnitude()));
+}
--- project2/geom2.h
+++ project2/geom2.h
@@ -18,30 +18,37 @@ int distance(int xA, int yA, int xB, int yB);
 // values of a velocity
 int pythagorean(int x, int y);
 
-
+// Conversion between degrees and radians
+float RadtoDeg(float rad);
+float DegtoRad(float deg);
 
 // Velocity class
 class Velocity {
  public:
-  Velocity() {}
+  Velocity();
+  Velocity(int x, int y);
   int getX() { return x; }
   int getY() { return y; }
-  float getMagnitude() { return magnitude; }
-  void setXY(int x, int y);
+  int getMagnitude() { return magnitude; }
   bool moving() { return (magnitude > 0); }
-  void slow(int t) { magnitude += ACCELERATION * t; }
+  void setXY(int x, int y);
+  void slow(int t);
  private:
   int x;
   int y;
   float directionRad;
-  float magnitude;
-  void updateMagnitude() { magnitude = ALPHA * pythagorean(x, y); }
+  int magnitude;
+  void setMagnitude() { magnitude = ALPHA * pythagorean(x, y); }
   void setDirectionRad() { directionRad = atan(y/x); }
+  void updateXY();
 };
+
+
+int DotProduct(Velocity vA, Velocity vB);
 
 // Uses the dot product between two velocity vectors to compute
 // the angle between them. Returns an angle in radians.
-float DotProduct(Velocity vA, Velocity vB);
+float AngleBetween(Velocity vA, Velocity vB);
 
 
 #endif
